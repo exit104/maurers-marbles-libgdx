5b7ea8b8bb231bcc1abeb75ecb650ed49cc33928
Daniel Uppenkamp <duppenkamp@gmail.com>
2020-05-08 23:30:51 -0400

    Code formatting updates.

--- core/src/com/exit104/maurersmarbles/libgdx/MaurersMarblesGame.java
+++ core/src/com/exit104/maurersmarbles/libgdx/MaurersMarblesGame.java
@@ -1,3 +1,8 @@
+/*
+ * You may only use this file in accordance with the terms and conditions
+ * outlined in the accompanying LICENSE file.
+ */
+
 package com.exit104.maurersmarbles.libgdx;
 
 import com.badlogic.gdx.ApplicationAdapter;
@@ -7,27 +12,29 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MaurersMarblesGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+  SpriteBatch batch;
+  Texture img;
+
+  @Override
+  public void create() {
+    batch = new SpriteBatch();
+    img = new Texture("badlogic.jpg");
+  }
+
+  @Override
+  public void render() {
+    Gdx.gl.glClearColor(1, 0, 0, 1);
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    batch.begin();
+    batch.draw(img, 0, 0);
+    batch.end();
+  }
+
+  @Override
+  public void dispose() {
+    batch.dispose();
+    img.dispose();
+  }
+
 }
--- desktop/src/com/exit104/maurersmarbles/libgdx/desktop/DesktopLauncher.java
+++ desktop/src/com/exit104/maurersmarbles/libgdx/desktop/DesktopLauncher.java
@@ -1,3 +1,8 @@
+/*
+ * You may only use this file in accordance with the terms and conditions
+ * outlined in the accompanying LICENSE file.
+ */
+
 package com.exit104.maurersmarbles.libgdx.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
@@ -5,8 +10,9 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.exit104.maurersmarbles.libgdx.MaurersMarblesGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MaurersMarblesGame(), config);
-	}
+
+  public static void main(String[] arg) {
+    LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+    new LwjglApplication(new MaurersMarblesGame(), config);
+  }
 }
