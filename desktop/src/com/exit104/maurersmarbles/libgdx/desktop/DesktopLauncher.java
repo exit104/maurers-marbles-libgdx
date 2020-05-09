d3a87afc6f46fb3bb7c6051b55f16174ac8b85cd
Daniel Uppenkamp <duppenkamp@gmail.com>
2020-05-08 23:14:13 -0400

    Initial commit.

---
+++ desktop/src/com/exit104/maurersmarbles/libgdx/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.exit104.maurersmarbles.libgdx.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.exit104.maurersmarbles.libgdx.MaurersMarblesGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MaurersMarblesGame(), config);
+	}
+}
